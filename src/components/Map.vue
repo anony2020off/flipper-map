15df43ce7f82cf3b72e4f4e904372d42305066bf
Levan Velijanashvili <stichoza@gmail.com>
2025-07-12 00:53:22 +0400

    Add hard to find hint

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -219,7 +219,7 @@ const createMarker = file => {
         <pre class="mt-2 card p-2 bg-body-secondary" style="max-height: 210px">${cleanContent}</pre>
       </details>`
       + (file.type === 'subghz' ? `<div class="mt-2">
-        <button class="btn btn-sm btn-secondary w-100 d-flex align-items-center" onclick="jsLaunchFile('${file.hash}')">
+        <button class="btn btn-sm btn-secondary w-100 d-flex align-items-center" onclick="jsLaunchFile('${file.hash}')" title="Flipper must be unlocked and apps should not be running">
           <span class="flex-grow-1 ps-3">Launch on Flipper</span>
           <i class="fas fa-square-arrow-up-right pull-right"></i></button>
       </div>` : '')
