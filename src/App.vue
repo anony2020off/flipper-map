3bef79e961c99435b5c91636ab5369f1a8931b51
Levan Velijanashvili <stichoza@gmail.com>
2025-07-13 02:10:32 +0400

    Move styles

--- src/App.vue
+++ src/App.vue
@@ -89,34 +89,3 @@ const handleSearch = (query) => {
     </div>
   </div>
 </template>
-
-<style>
-/* Import Bootstrap CSS */
-@import 'bootstrap/dist/css/bootstrap.min.css';
-
-/* Global styles */
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: Roboto, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-}
-
-.app-container {
-  height: 100%;
-  width: 100%;
-}
-
-.sidebar-col, .map-col {
-  height: 100%;
-  overflow: hidden;
-}
-</style>
